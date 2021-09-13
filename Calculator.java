d9bfffd2e4ec8adbc62a46df59ca57f9612a8f30
isultane <to_sultan@yahoo.com>
2021-09-13 14:13:21 +0300

    refactoring

--- Calculator.java
+++ Calculator.java
@@ -1,11 +1,4 @@
 
-
-/**
- * This is demo on how to use JUnit testing
- * @author Sultan S. Alqahtani
- * @version 1.0
- * Date: 13-09-2021
- */
 public class Calculator{
     public static void main(String[] args) {
         int a = 50;
@@ -16,12 +9,6 @@ public class Calculator{
         System.out.println("Max of a and b = "+ c);
     }
 
-    /**
-     * 
-     * @param x
-     * @param y
-     * @return the max between two intergers values
-     */
     public static int Max(int x, int y){
        if(x > y){
            return x;
