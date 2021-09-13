c35109d3b744d2e4e397cb16ec48b5010dd022ee
isultane <to_sultan@yahoo.com>
2021-09-13 13:59:17 +0300

    first commit

---
+++ Calculator.java
@@ -0,0 +1,42 @@
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ * This is demo on how to use JUnit testing
+ * @author Sultan S. Alqahtani
+ * @version 1.0
+ * Date: 13-09-2021
+ */
+public class Calculator{
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 60;
+
+        int c = Max(a, b);
+
+        System.out.println("Max of a and b = "+ c);
+    }
+
+    /**
+     * 
+     * @param x
+     * @param y
+     * @return the max between two intergers values
+     */
+    public static int Max(int x, int y){
+       if(x > y){
+           return x;
+       }
+       else{
+           return y;
+       }
+    }
+
+    @Test 
+    public void testAdd(){
+        assertEquals(3, Max(2,3));
+        assertEquals(10, Max(10,5));
+    }
+
+}
