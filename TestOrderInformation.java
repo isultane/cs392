deacd6b9b1672128f319bf073e1937286c6fd6e8
isultane <to_sultan@yahoo.com>
2021-09-28 00:14:46 +0300

    updated tutorial

--- TestOrderInformation.java
+++ TestOrderInformation.java
@@ -1,26 +1,4 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.Test;
 
 public class TestOrderInformation {
-    @Test
-    public void testOrderInformation(){
-        OrderInformation order = new OrderInformation();
-
-        order.setCustomerName("Sultan");
-        order.setGiftCardID(1234);
-        order.setOrderInformaion("American Coffee");
-        order.setReceiptInformation(1234656);
-
-        assertEquals("Sultan", order.getCustomerName());
-        assertEquals(1234, order.getGiftCardID());
-        assertEquals("American Coffee", order.getOrderInformaion());
-        assertEquals(1234656, order.getReceiptInformation());
-
-    }
-
-    @Test
-    public void testCreateOrderInformation(){
-        OrderInformation order = new OrderInformation();
-    }
+   
 }
