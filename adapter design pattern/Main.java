efdd6bf98b90f7b6aff3a19af0ac94e71cc9f2a4
isultane <to_sultan@yahoo.com>
2021-09-30 11:46:01 +0300

    adapter desgin pattern examples

---
+++ adapter design pattern/Main.java
@@ -0,0 +1,8 @@
+/**
+ * example reference from : https://www.vogella.com/tutorials/DesignPatternAdapter/article.html
+ */
+public class Main {
+    public static void main(String[] args) {
+       // complete 
+    }
+}
